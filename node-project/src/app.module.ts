56765fcb2b62a37e537bcf3de8a6875cc3fd52e1
NicoJMichel <nicolas.michel@enersis.ch>
2021-11-02 10:47:47 +0100

    feat(Node): add new controller

--- node-project/src/app.module.ts
+++ node-project/src/app.module.ts
@@ -5,11 +5,18 @@ import {
   ILYController,
   PostController,
 } from './app.controller';
+import { EmployeeController } from './controllers/employee.controller';
 import { AppService } from './app.service';
 
 @Module({
   imports: [],
-  controllers: [AppController, ByeController, ILYController, PostController],
+  controllers: [
+    AppController,
+    ByeController,
+    ILYController,
+    PostController,
+    EmployeeController,
+  ],
   providers: [AppService],
 })
 export class AppModule {}
---
+++ node-project/src/controllers/employee.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Get, Param, Post } from '@nestjs/common';
+import { get } from 'http';
+import { AppService } from '../app.service';
+
+@Controller('employee')
+export class EmployeeController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+}

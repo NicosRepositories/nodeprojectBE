7872e4f2d153cb3a995956ded0da0e1be8d3610a
NicoJMichel <nicolas.michel@enersis.ch>
2021-11-04 17:01:21 +0100

    feat(Node): remove copy-pasta code

--- node-project/src/mapper/employee.mapper.ts
+++ node-project/src/mapper/employee.mapper.ts
@@ -31,24 +31,4 @@ export class EmployeeMapper implements EmployeeRepository {
   createEmployee(employee: Employee): Promise<string> {
     throw new Error('Method not implemented.');
   }
-
-  /** Following Code is only for copy-pasta purposes and will be removed when finished.
-    
-    async getAllRequests(): Promise<AccountRequest[]> {
-    const queryResult: any = await this.dbConnection.reader.query(
-      "SELECT * FROM " + this.dbNames.requestFullPath,
-    );
-    const requestArray: AccountRequest[] = queryResult[0].map(
-      (request: any): AccountRequest => ({
-        id: request.rid,
-        username: request.username,
-        mail: request.mail,
-        firstName: request.firstname,
-        surname: request.surname,
-        regionId: request.home_region_id,
-        permission: this.reverseMapPermission(Number(request.permissions)),
-      }),
-    );
-    return requestArray;
-    */
 }

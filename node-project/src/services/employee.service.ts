6258c10a607b360d17edc88bea87cd157e17819d
NicoJMichel <nicolas.michel@enersis.ch>
2021-11-04 16:01:43 +0100

    feat(Node): getAllEmployees service

---
+++ node-project/src/services/employee.service.ts
@@ -0,0 +1,29 @@
+import { Inject, Injectable } from '@nestjs/common';
+import * as dotenv from 'dotenv';
+import { EmployeeRepository } from 'src/services/employee.repository';
+import { Employee, EmployeeDetail } from 'src/domain/employee';
+
+dotenv.config();
+
+/** Service that handles the Employees */
+@Injectable()
+export class EmployeeService {
+  constructor(
+    @Inject('EmployeeRepository')
+    private readonly employeeRepository: EmployeeRepository,
+  ) {}
+
+  async getAllEmployees(): Promise<Employee[]> {
+    const employee = await this.employeeRepository.getAllEmployees();
+
+    return employee;
+  }
+
+  async searchByName(lastName): Promise<EmployeeDetail[]> {
+    throw new Error('Method not implemented.');
+  }
+
+  async createEmployee(EmployeeDetail) {
+    throw new Error('Method not implemented.');
+  }
+}

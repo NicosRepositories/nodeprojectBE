6ba3bc6dc7d1d090e78a4895117cb8e3db1fd6e8
NicoJMichel <nicolas.michel@enersis.ch>
2021-11-15 14:53:08 +0100

    feat(Node): try to test contoller. not working

--- node-project/src/controllers/employee.controller.spec.ts
+++ node-project/src/controllers/employee.controller.spec.ts
@@ -1,22 +1,37 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { EmployeeController } from './employee.controller';
 import { AppService } from '../app.service';
+import { IMock, It, Mock, Times } from 'typemoq';
+import { Employee } from 'src/domain/employee';
+import { EmployeeService } from '../services/employee.service';
+import { sequelize } from 'database/models';
+import { EmployeeFactory } from 'src/domain/employee.factory';
 
-describe('EmployeeController', () => {
-  let employeeController: EmployeeController;
+test('that it creates the requested accountRequest', async () => {
+  // arrange
+  const employee: Employee = {
+    employeeID: 7788,
+    firstName: 'Unit',
+    lastName: 'Test',
+    nickName: 'test@gmail.com',
+    age: 44,
+    yearsAtEnersis: 1,
+    mainOffice: 'Testoffice',
+    happiness: 1,
+    jobID: 93,
+  };
+  const employeeServiceMock: IMock<EmployeeService> =
+    Mock.ofType<EmployeeService>();
+  employeeServiceMock
+    .setup((service) => service.createEmployee(employee))
+    .returns(async () => '7788');
 
-  beforeEach(async () => {
-    const app: TestingModule = await Test.createTestingModule({
-      controllers: [EmployeeController],
-      providers: [AppService],
-    }).compile();
+  // act
+  const sut: EmployeeController = new EmployeeController(
+    employeeServiceMock.object,
+  );
+  const result = await sut.createEmployee(employee);
 
-    employeeController = app.get<EmployeeController>(EmployeeController);
-  });
-
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(employeeController.getAllEmployees()).toBe('Hello World!');
-    });
-  });
+  // assert
+  expect(result.employeeId).toEqual('7788');
 });
